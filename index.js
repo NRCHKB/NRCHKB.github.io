2cd59907ae3490f5944cb225ef48b8cd3a6a7a3e
Shaquu <Shaquu@users.noreply.github.com>
2021-04-08 13:12:13 +0000

    deploy: 944caf53c52a4fe2a1e447ad0eb308d48d620a10

---
+++ index.js
@@ -0,0 +1,16 @@
+var userinput = document.getElementById('userinput');
+
+document.addEventListener('keydown', inputFocus);
+
+function inputFocus(e) {
+
+  if (e.keyCode === 191 ) {
+    e.preventDefault();
+    userinput.focus();
+  }
+
+  if (e.keyCode === 27 ) {
+    userinput.blur();
+  }
+
+}
